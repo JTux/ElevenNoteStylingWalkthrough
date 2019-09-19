13d02de5b7e62ddd900e2c2511760e034961d0ea
Joshua Tucker <thejltucker@gmail.com>
2019-09-19 11:07:44 -0400

    Category Controller and Views added

--- ElevenNote.Services/CategoryService.cs
+++ ElevenNote.Services/CategoryService.cs
@@ -53,7 +53,7 @@ namespace ElevenNote.Services
         {
             using (var ctx = new ApplicationDbContext())
             {
-                var entity = ctx.Categories.SingleOrDefault(e => e.CategoryId == categoryId);
+                var entity = ctx.Categories.SingleOrDefault(e => e.CategoryId == categoryId && e.CreatorId == _userId);
 
                 return
                     new CategoryDetail
